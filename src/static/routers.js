952a68b457c994b189cef65a37e46a5df2a87a57
Diego Santos <desenv.diegosantos@gmail.com>
2020-12-03 20:17:24 -0300

    Project structure

---
+++ src/static/routers.js
@@ -0,0 +1,5 @@
+export const APP = "/";
+export const TIMELINE = "/timeline";
+export const MY_PROFILE = "/meu-perfil";
+
+export const PRIVATE_ROUTERS = [TIMELINE, MY_PROFILE];
---
+++ src/static/urls.js
@@ -0,0 +1,10 @@
+const urls = {
+  ROUTES: {
+    LOGIN: `/`
+  },
+  LINKS: {
+    LOGIN: `/`
+  }
+};
+
+export default urls;
